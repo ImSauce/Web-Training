7cdc5119170f56d25043db916beba8582e4746e1
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-08-26 12:23:39 +0800

    1.0.3

---
+++ Quiz app/json.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+        question: "Who is the cutest hacker in the veritas club?",
+        answers: [
+            { text: "Hare Omagari", correct: true },
+            { text: "Maki Konuri", correct: false },
+            { text: "Kotama Otose", correct: false },
+            { text: "Chihiro Kagami", correct: false },
+        ]
+    },
+    {
+        question: "What school is Shiroko from?",
+        answers: [
+            { text: "Millennium Science School", correct: false },
+            { text: "Abydos High School", correct: true },
+            { text: "Gehenna Academy", correct: false },
+            { text: "Trinity General School", correct: false },
+        ]
+    },
+    {
+        question: "Which student is obsessed with peroro?",
+        answers: [
+            { text: "Aru Rikuhachima", correct: false },
+            { text: "Hifumi Ajitani", correct: true },
+            { text: "Hare Omagari", correct: false },
+            { text: "Mika", correct: false },
+        ]
+    },
+    {
+        question: "What’s Hare Omagari’s weapon?",
+        answers: [
+            { text: "Kel Tec RFB rifle", correct: true },
+            { text: "Sniper Rifle", correct: false },
+            { text: "Rocket Launcher", correct: false },
+            { text: "SMG", correct: false },
+        ]
+    },
+    {
+        question: "Which Abydos student loves robbing banks?",
+        answers: [
+            { text: "Shiroko", correct: true },
+            { text: "Ayane", correct: false },
+            { text: "Nonomi", correct: false },
+            { text: "Hoshino", correct: false },
+        ]
+    },
+    {
+        question: "Which student is broke and airhead?",
+        answers: [
+            { text: "Aru Rikuhachima", correct: true },
+            { text: "Hifumi Ajitani", correct: false },
+            { text: "Hare Omagari", correct: false },
+            { text: "Mika", correct: false },
+        ]
+    },
+     {
+        question: "What is Hoshino's favorite activity?",
+        answers: [
+            { text: "Napping and breeding", correct: true },
+            { text: "Studying hard", correct: false },
+            { text: "Managing Abydos finances", correct: false },
+            { text: "Beating Millennium in chess", correct: false },
+        ]
+    },
+    {
+        question: "Which club does Hare Omagari belong to?",
+        answers: [
+            { text: "Veritas", correct: true },
+            { text: "Justice Task Force", correct: false },
+            { text: "Cleaning & Clearing", correct: false },
+            { text: "Problem Solver 68", correct: false },
+        ]
+    },
+    {
+        question: "Which school is known for engineering and science?",
+        answers: [
+            { text: "Gehenna Academy", correct: false },
+            { text: "Millennium Science School", correct: true },
+            { text: "Trinity General School", correct: false },
+            { text: "Hyakkiyako Alliance Academy", correct: false },
+        ]
+    },
+    {
+        question: "Who would blow up Millennium’s servers just for fun?",
+        answers: [
+            { text: "Hare Omagari", correct: true },
+            { text: "Kotama", correct: false },
+            { text: "Maki", correct: false },
+            { text: "Shiroko", correct: false },
+        ]
+    }
+];
